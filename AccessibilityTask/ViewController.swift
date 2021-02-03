90856c2acfebf8d5fc8058eab3f2d09cb277f7c1
Mikhail Rubanov <m.rubanov@dodopizza.com>
2021-02-03 21:43:18 +0300

    Initial Commit

---
+++ AccessibilityTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityTask
+//
+//  Created by Mikhail Rubanov on 03.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
